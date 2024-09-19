a0609b6fb4b59e4321d061394346a516de1f26cb
Amanda Aguirre <amandamorenoaguirre@gmail.com>
2024-09-18 21:45:57 -0300

    Criacao da app mock

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {}
+
+variable "cluster_name" {}
+
+variable "service_name" {}
+
+variable "service_port" {}
+
+variable "service_cpu" {}
+
+variable "service_memory" {}
+
+variable "ssm_vpc_id" {}
+
+variable "ssm_listener" {}
+
+variable "ssm_private_subnet_1a" {}
+
+variable "ssm_private_subnet_1b" {}
+
+variable "ssm_private_subnet_1c" {}
+
+variable "environment_variables" {}
+
+variable "capabilities" {}
