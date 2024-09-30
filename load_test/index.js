97e73f38c4df46732ae590c364a52ec605149803
Amanda Aguirre <amandamorenoaguirre@gmail.com>
2024-09-30 20:05:40 -0300

    app

---
+++ load_test/index.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 5,
+  duration: '3000s',
+};
+
+const params = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Host': 'chip.linuxtips.demo'
+    },
+  };
+
+
+export default function () {
+  http.get('http://linuxtips-ecs-cluster-ingress-1148187578.us-east-1.elb.amazonaws.com/system', params);
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,12 @@ variable "service_memory" {}
 
 variable "service_health_check" {}
 
-variable "service_launch_type" {}
+variable "service_launch_type" {
+  type = list(object({
+    capacity_provider = string
+    weight            = number
+  }))
+}
 
 variable "service_task_count" {}
 
@@ -28,6 +33,54 @@ variable "ssm_private_subnet_1b" {}
 
 variable "ssm_private_subnet_1c" {}
 
-variable "environment_variables" {}
+variable "ssm_alb" {}
 
-variable "capabilities" {}
+variable "environment_variables" {
+  type = list(map(string))
+}
+
+variable "capabilities" {
+  type = list(string)
+}
+
+variable "scale_type" {}
+
+variable "task_minimum" {}
+
+variable "task_maximum" {}
+
+# Autoscaling de CPU
+
+variable "scale_out_cpu_threshold" {}
+
+variable "scale_out_adjustment" {}
+
+variable "scale_out_comparison_operator" {}
+
+variable "scale_out_statistic" {}
+
+variable "scale_out_period" {}
+
+variable "scale_out_evaluation_periods" {}
+
+variable "scale_out_cooldown" {}
+
+variable "scale_in_cpu_threshold" {}
+
+variable "scale_in_adjustment" {}
+
+variable "scale_in_comparison_operator" {}
+
+variable "scale_in_statistic" {}
+
+variable "scale_in_period" {}
+
+variable "scale_in_evaluation_periods" {}
+
+variable "scale_in_cooldown" {}
+
+# Tracking CPU
+variable "scale_tracking_cpu" {}
+
+# Tracking Request
+variable "scale_tracking_requests" {}
